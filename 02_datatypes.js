918c7b88f5bed73fe25c7cb92feedca1ab0f2f3b
Muhammad Bilal <150532599+MuhammadBilal039@users.noreply.github.com>
2024-10-30 15:12:22 +0000

    datatypes topic done

---
+++ 02_datatypes.js
@@ -0,0 +1,16 @@
+let name = "Bilal"
+let age = 22
+let isLoggedIn = true
+
+
+console.log(typeof age);
+
+
+// datatypes
+// string
+// number
+// boolean
+// null => typeof null is object
+// undefined
+// object
+// symbol
